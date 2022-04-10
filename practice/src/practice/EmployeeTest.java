d415e6032458afe0f04e6a45e9673edaf71eab5d
saranillo-throy <throy03@my.yorku.ca>
2022-04-10 18:20:01 -0400

    ayo?
    ayo?

--- practice/src/practice/EmployeeTest.java
+++ practice/src/practice/EmployeeTest.java
@@ -2,7 +2,31 @@ package practice;
 
 public class EmployeeTest {
 	public static void main (String args[]) {
-		Vegetable potato = new Vegetable ("Potato", 0, 1);
-		RootVegetable carrot = new RootVegetable("Taproot", "Carrot", 1, 1);
+		B test = new B("ayo");
+		A test1 = new A();
+		
+		System.out.println(A.getTest());
 	}
 }
+
+class B {
+	static String test;
+	public B(){
+		super();
+	}
+	public B(String test) {
+		super();
+		this.test = test;
+	}
+	
+	public static String getTest() {
+		return test + "ayo";
+	}
+}
+
+class A extends B{
+	public A() {
+		super();
+	}
+}
+
